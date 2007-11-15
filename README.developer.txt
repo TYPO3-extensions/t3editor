8bcf317ec3fc90ce79349907cad6adf34632537a
Tobias Liebig <mail@etobi.de>
2007-11-15 19:02:12 +0000

    clean up code (thanks to ingo renner)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3editor/trunk@7242 735d13b6-9817-0410-8766-e36946ffe9aa

--- README.developer.txt
+++ README.developer.txt
@@ -2,5 +2,5 @@
 * use the latest typo3core from svn (current trunk)
 * checkout t3editor (current trunk)
 * link the t3editor checkout folder to typo3/sysext/t3editor
-* apply core-patch from corepatch
+* apply the latest core-patch from corepatch/
 * aktivate t3editor from extension manager
--- class.tx_t3editor.php
+++ class.tx_t3editor.php
@@ -26,142 +26,165 @@
 ***************************************************************/
 
 /**
- * Provides a javascript-driven codeeditor with syntax highlighting for TS, HTML, CSS and more
+ * Provides a javascript-driven code editor with syntax highlighting for TS, HTML, CSS and more
  *
  * @author	Tobias Liebig <mail_typo3@etobi.de>
- *
  */
-
-
 class tx_t3editor {
+
+	/**
+	 * path to the main javascript-file
+	 *
+	 * @var string
+	 */
+	private $filepathEditorlib;
 	
-	var $filepath_editorlib = 'jslib/t3editor.js';
-	var $filepath_editorcss = 'css/t3editor.css';
+	/**
+	 * path to the main stylesheet
+	 *
+	 * @TODO: make it configurable
+	 *
+	 * @var string
+	 */
+	private $filepathEditorcss;
+
+	/**
+	 * counts the editors on the current page
+	 *
+	 * @var int
+	 */
+	private $editorCounter;
 	
-	var $editorCounter = 0;
-	var $isEnabled = true;	// set to false to totally switch off t3e 
-	
-	
-	function tx_t3editor() {
-			// check BEUser settings
-		$state = t3lib_div::_GP('t3editor_disableEditor')=='true' ? true : $GLOBALS['BE_USER']->uc['disableT3Editor'];
+	/**
+	 * flag to enable the t3editor
+	 *
+	 * @var bool
+	 */
+	private $isEnabled;
+
+
+	/**
+	 * constructor
+	 *
+	 * @return	void
+	 */
+	public function __construct() {
+
+		$this->filepathEditorlib = 'jslib/t3editor.js';
+		$this->filepathEditorcss = 'css/t3editor.css';
+		$this->editorCounter     = 0;
+		$this->isEnabled         = true; //TODO add a method to switch to false and turn off the editor completly
+
+			// check BE user settings
+			//TODO give $state a more descriptive name / state of/for what?
+		$state = t3lib_div::_GP('t3editor_disableEditor') == 'true' ? true : $GLOBALS['BE_USER']->uc['disableT3Editor'];
 		$this->setBEUCdisableT3Editor($state);
 	}
-	
-	
-	function setBEUCdisableT3Editor($state) {
+
+	/**
+	 * Enter description here...
+	 *
+	 * @param	boolean		$state
+	 * @return	void
+	 */
+	private function setBEUCdisableT3Editor($state) { //TODO better descriptive name for $state
 		if ($GLOBALS['BE_USER']->uc['disableT3Editor'] != $state) {
 			$GLOBALS['BE_USER']->uc['disableT3Editor'] = $state;
+
 			$GLOBALS['BE_USER']->writeUC();
 		}
 	}
-	
-	
-	function getCodeJS() {
 
-		if ($this->isEnabled)	{
-			
-				// disable the obsolete tab.js to avoid conflict
+	/**
+	 * Enter description here...
+	 *
+	 * @return	string		DESCRIPTION GOES HERE
+	 */
+	public function getJavascriptCode() {
+		$code = ''; //TODO find a more descriptive name (low prio)
+
+		if($this->isEnabled) {
+				// disable the obsolete tab.js to avoid conflicts
 			$GLOBALS['BE_USER']->uc['disableTabInTextarea'] = '1';
-			
-			$path_t3e = $GLOBALS['BACK_PATH'].
-				t3lib_extmgm::extRelPath('t3editor');
-			
-			$code = '';
+
+			$path_t3e = $GLOBALS['BACK_PATH'].t3lib_extmgm::extRelPath('t3editor');
+
 			$code.= '<script type="text/javascript">'.
 				'var PATH_t3e = "'.$GLOBALS['BACK_PATH']. t3lib_extmgm::extRelPath('t3editor').'"; '.
-				$debug.
 				'</script>';
-			
+
 			$code.= '<script src="'.$path_t3e.'/jslib/Mochi.js" type="text/javascript"></script>'.
-     			'<script src="'.$path_t3e.'/jslib/util.js" type="text/javascript"></script>'.
-     			'<script src="'.$path_t3e.'/jslib/select.js" type="text/javascript"></script>'.
+				'<script src="'.$path_t3e.'/jslib/util.js" type="text/javascript"></script>'.
+				'<script src="'.$path_t3e.'/jslib/select.js" type="text/javascript"></script>'.
 				'<script src="'.$path_t3e.'/jslib/stringstream.js" type="text/javascript"></script>'.
 				'<script src="'.$path_t3e.'/jslib/parsetyposcript.js" type="text/javascript"></script>'.
-     			'<script src="'.$path_t3e.'/jslib/tokenizetyposcript.js" type="text/javascript"></script>';
+				'<script src="'.$path_t3e.'/jslib/tokenizetyposcript.js" type="text/javascript"></script>';
 
-				// include prototype-js-lib 
-			$code.= '<script src="'.
-				$GLOBALS['BACK_PATH'].
-				'contrib/prototype/prototype.js'.
-				'" type="text/javascript" id="prototype-script"></script>';
-			
-				// include scriptaculous-lib 
-			$code.= '<script src="'.
-				$GLOBALS['BACK_PATH'].
-				'contrib/scriptaculous/scriptaculous.js'.
-				'" type="text/javascript" id="scriptaculous-script"></script>';
-			// $code.= '<script src="'.
-			//	$GLOBALS['BACK_PATH'].
-			//	'contrib/scriptaculous/resizable.js'.
-			//	'" type="text/javascript" id="scriptaculous-script"></script>';
-			
+				// include prototype and scriptacolous
+				// TODO: should use the new loadJavascriptLib
+			$code.= '<script src="'.$GLOBALS['BACK_PATH'].'contrib/prototype/prototype.js" type="text/javascript" id="prototype-script"></script>';
+			$code.= '<script src="'.$GLOBALS['BACK_PATH'].'contrib/scriptaculous/scriptaculous.js" type="text/javascript" id="scriptaculous-script"></script>';
+
 				// include editor-css
-        	$code.= '<link href="'.
-				$GLOBALS['BACK_PATH'].
-				t3lib_extmgm::extRelPath('t3editor').
-				 $this->filepath_editorcss.
-				'" type="text/css" rel="stylesheet" />';
-			
+			$code.= '<link href="'.$GLOBALS['BACK_PATH'].t3lib_extmgm::extRelPath('t3editor').$this->filepathEditorcss.'" type="text/css" rel="stylesheet" />';
+
 				// include editor-js-lib
-			$code.= '<script src="'.
-				$GLOBALS['BACK_PATH'].
-				t3lib_extmgm::extRelPath('t3editor').
-				$this->filepath_editorlib.
-				'" type="text/javascript" id="t3editor-script"></script>';
-						
-			return $code;
-		} else {
-			return '';
+			$code.= '<script src="'.$GLOBALS['BACK_PATH'].t3lib_extmgm::extRelPath('t3editor').$this->filepathEditorlib.'" type="text/javascript" id="t3editor-script"></script>';
 		}
+
+		return $code;
 	}
-	
-	
-	function getCodeEditor($name, $class='', $content='', $additionalParams='', $alt='') {
+
+	/**
+	 * Enter description here...
+	 *
+	 * @param	unknown_type		$name
+	 * @param	unknown_type		$class
+	 * @param	unknown_type		$content
+	 * @param	unknown_type		$additionalParams
+	 * @param	unknown_type		$alt
+	 * @return	string		DESCRIPTION GOES HERE
+	 */
+	public function getCodeEditor($name, $class='', $content='', $additionalParams='', $alt='') {
 		$code = '';
-		
+
 		if ($this->isEnabled) {
-			
 			$this->editorCounter++;
-			
+
 			$class .= ' t3editor';
-					
-			$code.= '<div><textarea '.
-				'id="t3editor_'.$this->editorCounter.'" '.
-				'name="'.$name.'" '.
-				(!empty($class)?'class="'.$class.'" ':'').
-				$additionalParams.
-				($alt!=''?' alt="'.$alt.'"':'').
-				'>'.
-				$content.
-				'</textarea></div>';
-			
+			if(!empty($alt)) {
+				$alt = ' alt="'.$alt.'"';
+			}
+
+			$code.= '<div>
+				<textarea id="t3editor_'.$this->editorCounter.'" name="'.$name.'" class="'.$class.'" '.$additionalParams.' '.$alt.'>'
+				.$content
+				.'</textarea></div>';
+
 			$checked = $GLOBALS['BE_USER']->uc['disableT3Editor'] ? 'checked="checked"' : '';
-			$code.= '<br/><br/><input type="checkbox" onclick="t3editor_toggleEditor(this);" '.
-				' name="t3editor_disableEditor" value="true" id="t3editor_disableEditor_'.$this->editorCounter.'_checkbox" '.$checked.' />'.
-				'<label for="t3editor_disableEditor_'.$this->editorCounter.'_checkbox">'.
-				'deactivate t3editor</label><br/><br/>';
-			
+			$code.= '<br/><br/>
+				<input type="checkbox" onclick="t3editor_toggleEditor(this);" name="t3editor_disableEditor" value="true" id="t3editor_disableEditor_'.$this->editorCounter.'_checkbox" '.$checked.' />&nbsp;
+				<label for="t3editor_disableEditor_'.$this->editorCounter.'_checkbox">deactivate t3editor</label>
+				<br/><br/>';
+
 		} else {
 				// fallback
-			$code.= '<textarea '.
-				'name="'.$name.'" '.
-				(!empty($class)?'class="'.$class.'" ':'').
-				$additionalParams.
-				'>'.
-				$content.
-				'</textarea>';
+			if(!empty($class)) {
+				$class = 'class="'.$class.'" ';
+			}
+
+			$code.= '<textarea name="'.$name.'" '.$class.$additionalParams.'>'.$content.'</textarea>';
 		}
-		
+
 		return $code;
 	}
 
 }
 
 
-// Include extension?
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['sysext/t3editor/class.tx_t3editor.php'])   {
-        include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['sysext/t3editor/class.tx_t3editor.php']);
+	// Include extension?
+if(defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['sysext/t3editor/class.tx_t3editor.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['sysext/t3editor/class.tx_t3editor.php']);
 }
 
 
